8cd838c93326ec28e44237aacfd5983c3c535430
apolloclark <apolloclark@gmail.com>
2019-06-03 18:03:51 -0400

    updated to python 3.7.3

--- build_packer_docker_centos7.6.sh
+++ build_packer_docker_centos7.6.sh
@@ -4,7 +4,7 @@
 export DOCKER_USERNAME=${DOCKER_USERNAME:=$(whoami)}
 export PACKAGE=${PACKAGE:=pacu}
 export PACKAGE_VERSION=${PACKAGE_VERSION:=0.0.1}
-export BASE_IMAGE=${BASE_IMAGE:="apolloclark/python3:3.7.2-centos7.6"}
+export BASE_IMAGE=${BASE_IMAGE:="apolloclark/python3:3.7.3-centos7.6"}
 export IMAGE_NAME=${IMAGE_NAME:="centos7.6"}
 
 ./build_packer_docker.sh
--- build_packer_docker_ubuntu16.04.sh
+++ build_packer_docker_ubuntu16.04.sh
@@ -4,7 +4,7 @@
 export DOCKER_USERNAME=${DOCKER_USERNAME:=$(whoami)}
 export PACKAGE=${PACKAGE:=pacu}
 export PACKAGE_VERSION=${PACKAGE_VERSION:=0.0.1}
-export BASE_IMAGE=${BASE_IMAGE:="apolloclark/python3:3.7.2-ubuntu16.04"}
+export BASE_IMAGE=${BASE_IMAGE:="apolloclark/python3:3.7.3-ubuntu16.04"}
 export IMAGE_NAME=${IMAGE_NAME:="ubuntu16.04"}
 
 ./build_packer_docker.sh
