6c77f78f2f2afc0a5ecd22a553491ae4863403eb
apolloclark <apolloclark@gmail.com>
2019-09-14 14:50:47 -0400

    removed old build scripts, added "install_post.sh"

--- build_packer_docker_all.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -eux
-
-# ensure that ENV VARs are set
-export DOCKER_USERNAME=${DOCKER_USERNAME:=$(whoami)}
-export PACKAGE=${PACKAGE:=pacu}
-export PACKAGE_VERSION=${PACKAGE_VERSION:=0.0.1}
-
-./build_packer_docker_ubuntu16.04.sh
-./build_packer_docker_centos7.6.sh
